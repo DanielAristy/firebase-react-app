9b243aaad5d0bb98e3d0e664c1a551f4458a8e2a
DanielAristy <danielaristy22@gmail.com>
2021-07-01 20:15:44 -0500

    Configuracion desde cero de Firebase en React

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDpic63eJrjoN4cZG0wFEZXG-FbgXM2bDU",
+    authDomain: "reactfirebasedemos-587f7.firebaseapp.com",
+    projectId: "reactfirebasedemos-587f7",
+    storageBucket: "reactfirebasedemos-587f7.appspot.com",
+    messagingSenderId: "607771467167",
+    appId: "1:607771467167:web:bd8fb08ef7c682e3aab1aa",
+    measurementId: "G-BWMQW90J0B"
+};
